db7913619149469ef1723aa715a9d4fa0bd549f4
Melody <melody@aquarne.cc>
2024-09-28 17:29:22 +0200

    Added more Thunderbird settings.

--- home/dev/programs/social/thunderbird/default.nix
+++ home/dev/programs/social/thunderbird/default.nix
@@ -2,9 +2,18 @@ _:
 {
   programs.thunderbird = {
     enable = true;
+    settings = {
+      "extensions.activeThemeID" = "thunderbird-compact-dark@mozilla.org";
+      "mailnews.default_sort_type" = "18";
+      "mailnews.default_sort_order" = "2";
+      "mailnews.default_sort_view" = "1";
+    };
     profiles = {
       "main" = {
         isDefault = true;
+        settings = {
+          "intl.locale.requested" = "de,en-US";
+        };
       };
     };
   };
