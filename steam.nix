145cebe5cb3b413a6f697ae8c29a1c7be9e18474
Melody <melody@aquarne.cc>
2024-09-28 17:29:23 +0200

    Oops, didn't see this option! Feral's GameMode should have been enabled with programs.gamemode.enable instead of adding it to environtment.systemPackages.

--- steam.nix
+++ steam.nix
@@ -1,13 +1,11 @@
-{ pkgs, ... }:
+{ ... }:
 {
-  programs.steam = {
-    enable = true;
-    dedicatedServer.openFirewall = true;
-    remotePlay.openFirewall = true;
+  programs = {
+    gamemode.enable = true;
+    steam = {
+      enable = true;
+      dedicatedServer.openFirewall = true;
+      remotePlay.openFirewall = true;
+    };
   };
-
-  environment.systemPackages = with pkgs;
-    [
-      gamemode
-    ];
 }
