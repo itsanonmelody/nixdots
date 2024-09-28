22f833a4a7ceefc8695941969df4e3feab29bba5
Melody <melody@aquarne.cc>
2024-09-28 17:29:24 +0200

    Added drop-in shell capability to Yazi.

--- home/dev/programs/file-manager/yazi/default.nix
+++ home/dev/programs/file-manager/yazi/default.nix
@@ -2,5 +2,14 @@ _:
 {
 	programs.yazi = {
 		enable = true;
+		keymap = {
+			manager.prepend_keymap = [
+				{
+					on   = [ "<C-s>" ];
+					run  = "shell \"$SHELL\" --block --confirm";
+					desc = "Open shell here";
+				}
+			];
+		};
 	};
 }
