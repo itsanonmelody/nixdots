c11024266f7ccb88936d236cb04b1050323af2c9
Melody <melody@aquarne.cc>
2024-09-28 17:29:27 +0200

    Added overlay to xivlauncher, so that it detects libgamemodeauto.so.0.

--- home/dev/programs/game/xivlauncher/default.nix
+++ home/dev/programs/game/xivlauncher/default.nix
@@ -1,4 +1,10 @@
-{ pkgs, ... }:
+{ osConfig, lib, pkgs, ... }:
+lib.warnIf (!osConfig.programs.gamemode.enable)
+  ''
+    Feral's GameMode CPU optimisations are required to run XIVLauncher.Core properly!
+    To enable it, add the following option in the NixOS configuration:
+      > programs.gamemode.enable = true;
+  ''
 {
   home.packages = with pkgs;
     [
@@ -7,4 +13,25 @@
       wineWowPackages.stableFull
       xivlauncher
     ];
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      xivlauncher = prev.xivlauncher.overrideAttrs {
+        # XIVLauncher requires libgamemodeauto.o.0 to enable
+        # Feral's GameMode CPU optimisations.
+        desktopItems = [
+          # Taken from https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/by-name/xi/xivlauncher/package.nix
+          (pkgs.makeDesktopItem {
+            name = "xivlauncher";
+            exec = "gamemoderun XIVLauncher.Core";
+            icon = "xivlauncher";
+            desktopName = "XIVLauncher";
+            comment = prev.xivlauncher.meta.description;
+            categories = [ "Game" ];
+            startupWMClass = "XIVLauncher.Core";
+          })
+        ];
+      };
+    })
+  ];
 }
