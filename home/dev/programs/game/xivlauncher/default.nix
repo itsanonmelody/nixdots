3d302bb9e08c48fd9d9ce4c68be5e8f9b3398616
Melody <melody@aquarne.cc>
2024-09-28 17:29:28 +0200

    Made xivlauncher overlay optional. Only apply the xivlauncher overlay that enables Feral's GameMode CPU optimisations when gamemode is enabled in the system configuration. This makes sure that even if gamemode is not installed, xivlauncher can still run.

--- home/dev/programs/game/xivlauncher/default.nix
+++ home/dev/programs/game/xivlauncher/default.nix
@@ -1,7 +1,7 @@
 { osConfig, lib, pkgs, ... }:
 lib.warnIf (!osConfig.programs.gamemode.enable)
   ''
-    Feral's GameMode CPU optimisations are required to run XIVLauncher.Core properly!
+    Feral's GameMode CPU optimisations are required to run XIVLauncher.Core optimally!
     To enable it, add the following option in the NixOS configuration:
       > programs.gamemode.enable = true;
   ''
@@ -15,23 +15,26 @@ lib.warnIf (!osConfig.programs.gamemode.enable)
     ];
 
   nixpkgs.overlays = [
-    (final: prev: {
-      xivlauncher = prev.xivlauncher.overrideAttrs {
-        # XIVLauncher requires libgamemodeauto.o.0 to enable
-        # Feral's GameMode CPU optimisations.
-        desktopItems = [
-          # Taken from https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/by-name/xi/xivlauncher/package.nix
-          (pkgs.makeDesktopItem {
-            name = "xivlauncher";
-            exec = "gamemoderun XIVLauncher.Core";
-            icon = "xivlauncher";
-            desktopName = "XIVLauncher";
-            comment = prev.xivlauncher.meta.description;
-            categories = [ "Game" ];
-            startupWMClass = "XIVLauncher.Core";
-          })
-        ];
-      };
-    })
+    (if (osConfig.programs.gamemode.enable)
+      then
+      (final: prev: {
+        xivlauncher = prev.xivlauncher.overrideAttrs {
+          # XIVLauncher requires libgamemodeauto.o.0 to enable
+          # Feral's GameMode CPU optimisations.
+          desktopItems = [
+            # Taken from https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/by-name/xi/xivlauncher/package.nix
+            (pkgs.makeDesktopItem {
+              name = "xivlauncher";
+              exec = "gamemoderun XIVLauncher.Core";
+              icon = "xivlauncher";
+              desktopName = "XIVLauncher";
+              comment = prev.xivlauncher.meta.description;
+              categories = [ "Game" ];
+              startupWMClass = "XIVLauncher.Core";
+            })
+          ];
+        };
+      })
+      else (_: _: {}))
   ];
 }
