c5f79c6001c9101776c7460d6d2fa922d9b68bd3
Melody <melody@aquarne.cc>
2025-01-03 16:03:51 +0100

    Removed password declaration from users.

--- hosts/laptop/users.nix
+++ hosts/laptop/users.nix
@@ -1,24 +1,9 @@
 { config, lib, pkgs, ... }:
-let
-  user-secrets = builtins.fromTOML (builtins.readFile ./user-secrets.toml);
-  getUserPassword = user:
-    if (builtins.hasAttr "password" user-secrets.${user})
-      && !(builtins.hasAttr "hashedPassword" user-secrets.${user})
-    then user-secrets.${user}.password
-    else null;
-  getUserHashedPassword = user:
-    if (builtins.hasAttr "hashedPassword" user-secrets.${user})
-    then user-secrets.${user}.hashedPassword
-    else null;
-in
 {
-  users.mutableUsers = false;
   users.users = {
     root = {
       isSystemUser = true;
       home = "/root";
-      password = getUserPassword "root";
-      hashedPassword = getUserHashedPassword "root";
     };
     dev = {
       isNormalUser = true;
@@ -32,8 +17,6 @@ in
           "video"
           "wheel"
         ];
-      password = getUserPassword "dev";
-      hashedPassword = getUserHashedPassword "dev";
     };
   };
 }
