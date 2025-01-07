3b981cdeeffbd67a468350aaecd774725ca8b444
Melody <melody@aquarne.cc>
2025-01-07 21:38:47 +0100

    Make users immutable.

--- hosts/laptop/users.nix
+++ hosts/laptop/users.nix
@@ -1,22 +1,24 @@
 { config, lib, pkgs, ... }:
 {
+  users.mutableUsers = false;
   users.users = {
     root = {
       isSystemUser = true;
       home = "/root";
+      hashedPasswordFile = "/etc/nixos/password/root";
     };
     dev = {
       isNormalUser = true;
       home = "/home/dev";
-      extraGroups =
-        [
-          "gamemode"
-          "lp"
-          "networkmanager"
-          "scanner"
-          "video"
-          "wheel"
-        ];
+      extraGroups = [
+        "gamemode"
+        "lp"
+        "networkmanager"
+        "scanner"
+        "video"
+        "wheel"
+      ];
+      hashedPasswordFile = "/etc/nixos/password/dev";
     };
   };
 }
