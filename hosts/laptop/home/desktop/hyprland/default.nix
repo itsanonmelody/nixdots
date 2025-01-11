feb4a4def99076361249743c9074fbabae91c6de
Melody <melody@aquarne.cc>
2025-01-11 23:38:04 +0100

    Added XDG user directiores environment variables.

--- hosts/laptop/home/desktop/hyprland/default.nix
+++ hosts/laptop/home/desktop/hyprland/default.nix
@@ -42,6 +42,15 @@ in
         ''
           export XCURSOR_THEME=Adwaita
           export XCURSOR_SIZE=24
+
+          export XDG_DESKTOP_DIR="$HOME/Desktop"
+          export XDG_DOCUMENTS_DIR="$HOME/Documents"
+          export XDG_DOWNLOAD_DIR="$HOME/Downloads"
+          export XDG_MUSIC_DIR="$HOME/Music"
+          export XDG_PICTURES_DIR="$HOME/Pictures"
+          export XDG_PUBLICSHARE_DIR="$HOME/Public"
+          export XDG_TEMPLATES_DIR="$HOME/Templates"
+          export XDG_VIDEOS_DIR="$HOME/Videos"
         '';
     };
     ".config/hypr/hyprland.conf" = {
