9890077a38831c7805afb947c70ff675ce5ead8e
Melody <melody@aquarne.cc>
2025-01-11 19:32:15 +0100

    Enabled UWSM for Hyprland.

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -175,6 +175,7 @@
     zsh.enable = true;
     hyprland = {
       enable = true;
+      withUWSM = true;
       package = inputs.hyprland.packages.${pkgs.system}.hyprland;
       portalPackage = inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland;
     };
