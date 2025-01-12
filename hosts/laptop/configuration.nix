7fb482238d7829d3382bd0c15abb94f7b44aee9a
Melody <melody@aquarne.cc>
2025-01-12 19:05:25 +0100

    Added basic plymouth config.

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -38,6 +38,30 @@
         '';
       };
     };
+    plymouth = {
+      enable = true;
+      theme = "circle_hud";
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes.override {
+          selected_themes = [
+            "circle_hud"
+            "loader_2"
+          ];
+        })
+      ];
+    };
+    initrd.verbose = false;
+
+    consoleLogLevel = 0;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "loglevel=3"
+      "rd.systemd.show_status=false"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
   };
 
   hardware = {
