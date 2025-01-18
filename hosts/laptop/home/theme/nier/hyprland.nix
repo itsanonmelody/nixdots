7c16545149300c2f16b35c0c8717e206a599c52e
Melody <melody@aquarne.cc>
2025-01-18 16:52:17 +0100

    Changed active border and group bar color.

--- hosts/laptop/home/theme/nier/hyprland.nix
+++ hosts/laptop/home/theme/nier/hyprland.nix
@@ -18,7 +18,7 @@ in
             gaps_in = 0
             gaps_out = 0
             col.inactive_border = rgb(${toHexStringRgb colors.main})
-            col.active_border = rgb(${toHexStringRgb colors.secondaryShade})
+            col.active_border = rgb(${toHexStringRgb colors.mainTint})
             
           }
 
@@ -35,19 +35,19 @@ in
           animations:enabled = false
 
           group {
-            col.border_active = rgb(${toHexStringRgb colors.secondaryShade})
+            col.border_active = rgb(${toHexStringRgb colors.mainTint})
             col.border_inactive = rgb(${toHexStringRgb colors.main})
-            col.border_locked_active = rgb(${toHexStringRgb colors.secondaryShade})
+            col.border_locked_active = rgb(${toHexStringRgb colors.mainTint})
             col.border_locked_inactive = rgb(${toHexStringRgb colors.main})
 
             groupbar {
               enabled = true
               font_size = 12
               height = 20
-              text_color = rgb(${toHexStringRgb colors.secondaryShade})
-              col.active = rgb(${toHexStringRgb colors.secondaryShade})
+              text_color = rgb(${toHexStringRgb colors.mainTint})
+              col.active = rgb(${toHexStringRgb colors.mainTint})
               col.inactive = rgb(${toHexStringRgb colors.main})
-              col.locked_active = rgb(${toHexStringRgb colors.secondaryShade})
+              col.locked_active = rgb(${toHexStringRgb colors.mainTint})
               col.locked_inactive = rgb(${toHexStringRgb colors.main})
             }
           }
