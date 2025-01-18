760cd052ae27f4a09feb432ee0168d6c38790de9
Melody <melody@aquarne.cc>
2025-01-18 03:59:37 +0100

    Added local to flake outputs for debugging convenience.

--- flake.nix
+++ flake.nix
@@ -18,6 +18,7 @@
       local = import ./local inputs;
     in
       {
+        local = local;
         nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           specialArgs = { inherit inputs; inherit local; };
