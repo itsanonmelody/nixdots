0e32c9cf8a5a2601dfe09052c9c0a649c025ba74
Melody <melody@aquarne.cc>
2025-06-09 00:54:59 +0200

    Added drm revert patch for the amdgpu kernel module.

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -1,4 +1,4 @@
-{ inputs, local, pkgs, lib, ... }:
+{ inputs, local, config, pkgs, lib, ... }:
 {
   imports = [
     inputs.hjem.nixosModules.default
@@ -20,6 +20,61 @@
       };
     })
   ];
+
+  boot.extraModulePackages =
+    let
+      inherit (config.boot.kernelPackages) kernel;
+      amdgpu-hang-fix = pkgs.fetchpatch {
+        name = "amdgpu-hang-fix";
+        url = "https://gitlab.freedesktop.org/agd5f/linux/-/commit/1b824eef269db44d068bbc0de74c94a8e8f9ce02.diff";
+        hash = "sha256-LsmxcfL+ujHfTw+s7PMaRGYhuvCUWrc8vYhu6yMSzno=";
+      };
+      amdgpu-kernel-module =
+        pkgs.stdenv.mkDerivation {
+          pname = "amdgpu-kernel-module";
+          inherit (kernel)
+            src
+            version
+            postPatch
+            nativeBuildInputs;
+
+          kernel_dev = kernel.dev;
+          kernelVersion = kernel.modDirVersion;
+
+          modulePath = "drivers/gpu/drm/amd/amdgpu";
+
+          buildPhase =
+            ''
+              BUILT_KERNEL=$kernel_dev/lib/modules/$kernelVersion/build
+
+              cp $BUILT_KERNEL/Module.symvers .
+              cp $BUILT_KERNEL/.config        .
+              cp $kernel_dev/vmlinux          .
+
+              make "-j$NIX_BUILD_CORES" modules_prepare
+              make "-j$NIX_BUILD_CORES" M=$modulePath modules
+            '';
+
+          installPhase =
+            ''
+              make \
+                INSTALL_MOD_PATH="$out" \
+                XZ="xz -T$NIX_BUILD_CORES" \
+                M="$modulePath" \
+                modules_install
+            '';
+
+          meta = {
+            description = "AMD GPU kernel module";
+            license = lib.licenses.gpl3;
+          };
+        };
+    in
+      [
+        (amdgpu-kernel-module.overrideAttrs {
+          patches = [ amdgpu-hang-fix ];
+        })
+      ];
 
   boot = {
     loader = {
